a8cdd6c641a00828bcdc1007a76aab7de3a3848e
Anuar Ahouari <anuarmohaseca@gmail.com>
2016-10-04 13:32:25 +0100

    Added: Histogram contructor and getter

---
+++ src/Histogram.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Anuar on 04/10/2016.
+ */
+public class Histogram {
+
+    private ArrayList<Integer> histogram;
+
+    public Histogram(ArrayList<Integer> histogram){
+        this.histogram = histogram;
+    }
+
+    public ArrayList<Integer> getHistogram() {
+        return histogram;
+    }
+}
